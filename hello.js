23963ab9bf80c462abebf475e284840fc0a5b4d5
dywu <dingyuan.wu@macrosilicon.com>
2022-03-08 10:57:21 +0800

    Modify *.c to *.js file

--- hello.js
+++ hello.js
@@ -1,3 +1,6 @@
+/*
+ cron "15 6-18/6 * * *" hello.js,tag=hello
+ */
 #include <stdio.h>
 
 int main(void)
---
+++ main.js
@@ -0,0 +1,10 @@
+/*
+ cron "15 6-18/6 * * *" script-path=main.js,tag=main
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    return 1;
+}
